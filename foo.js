0cfec50fe76d8382db2bbbb48762f24cac430359
Agene Jhee <ajhee.one@gmail.com>
2021-03-11 19:44:34 +0900

    first problem solving

--- foo.js
+++ foo.js
@@ -1,3 +1,11 @@
-let foo = "bar";
-console.log(foo);
-foo;
+let num = 4936;
+
+let ones = num % 10;
+let tens = (num - ones) % 100 / 10;
+let hundreds = (num - (tens*10) - ones ) % 1000 / 100;
+let thousands = Math.trunc(num / 1000)
+
+console.log(thousands);
+console.log(hundreds);
+console.log(tens);
+console.log(ones);   // 6
