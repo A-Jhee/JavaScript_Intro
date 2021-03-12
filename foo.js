c1793ae8dc89aef74974340827c730c116d08564
Agene Jhee <ajhee.one@gmail.com>
2021-03-12 18:50:38 +0900

    more exercises

--- foo.js
+++ foo.js
@@ -1,11 +1,6 @@
-let num = 4936;
+const FOO = 'bar';
+{
+  const FOO = 'qux';
+}
 
-let ones = num % 10;
-let tens = (num - ones) % 100 / 10;
-let hundreds = (num - (tens*10) - ones ) % 1000 / 100;
-let thousands = Math.trunc(num / 1000)
-
-console.log(thousands);
-console.log(hundreds);
-console.log(tens);
-console.log(ones);   // 6
+console.log(FOO);
